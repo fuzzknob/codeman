ee30850e164a78c118a20a36e03effacd3e5da28
Gagan Rai <gagan_rai@outlook.com>
2021-11-09 00:06:59 +0545

    init: add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
-ReactDom.render(<App />, document.getElementById('codeman-app-root'))
+ReactDom.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('codeman-app-root'),
+)
