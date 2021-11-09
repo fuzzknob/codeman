92e379379c0fa8127eca242082de850c8793094a
Gagan Rai <gagan_rai@outlook.com>
2021-11-09 23:33:19 +0545

    fix: height issue

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="bg-primary dark:bg-primary-dark text-color-dark dark:text-color-white rounded-lg h-screen select-none cursor-default text-base flex flex-col">
       <TitleBar />
-      <main className="overflow-auto flex-1 flex flex-col">
+      <main className="h-full overflow-auto">
         <Routes />
       </main>
     </div>
--- src/screens/Setup/SetupScreen.tsx
+++ src/screens/Setup/SetupScreen.tsx
@@ -3,7 +3,7 @@ import GitSetup from './GitSetup'
 
 const SetupScreen: React.FC = () => {
   return (
-    <section className="pt-5 pb-14 w-7/12 flex-1 mx-auto flex flex-col justify-between">
+    <section className="pt-5 pb-14 w-7/12 h-full mx-auto flex flex-col justify-between">
       <h1 className="text-2xl text-center">Setup</h1>
       {/* <Cli /> */}
       <GitSetup />
